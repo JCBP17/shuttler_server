dddc8128f1fe657fc7e03229e82718d8491e2366
osias10 <wnsguddk1@naver.com>
2022-05-08 02:11:37 +0900

    Spring boot project create

---
+++ shuttler_server/src/test/java/com/osias10/shuttlerserver/ShuttlerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osias10.shuttlerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttlerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
